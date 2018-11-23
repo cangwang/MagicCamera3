7876514da3457d9a8c2b7668321bc1757505d2a7
cangwang <284699931@qq.com>
2018-11-23 16:45:36 +0800

    [2018.11.23]添加Amaro和Antique滤镜

---
+++ app/src/main/assets/amaro.glsl
@@ -0,0 +1,34 @@
+#version 300 es
+precision mediump float;
+ 
+ in mediump vec2 textureCoordinate;
+ 
+ uniform sampler2D inputImageTexture;
+ uniform sampler2D inputImageTexture2; //blowout;
+ uniform sampler2D inputImageTexture3; //overlay;
+ uniform sampler2D inputImageTexture4; //map
+ 
+ uniform float strength;
+
+ layout(location=0) out vec4 fragColor;
+
+ void main()
+ {
+     vec4 originColor = texture(inputImageTexture, textureCoordinate);
+     vec4 texel = texture(inputImageTexture, textureCoordinate);
+     vec3 bbTexel = texture(inputImageTexture2, textureCoordinate).rgb;
+     
+     texel.r = texture(inputImageTexture3, vec2(bbTexel.r, texel.r)).r;
+     texel.g = texture(inputImageTexture3, vec2(bbTexel.g, texel.g)).g;
+     texel.b = texture(inputImageTexture3, vec2(bbTexel.b, texel.b)).b;
+     
+     vec4 mapped;
+     mapped.r = texture(inputImageTexture4, vec2(texel.r, .16666)).r;
+     mapped.g = texture(inputImageTexture4, vec2(texel.g, .5)).g;
+     mapped.b = texture(inputImageTexture4, vec2(texel.b, .83333)).b;
+     mapped.a = 1.0;
+     
+     mapped.rgb = mix(originColor.rgb, mapped.rgb, strength);
+
+     gl_FragColor = mapped;
+ }
---
+++ app/src/main/cpp/filter/advanced/MagicAmaroFilter.cpp
@@ -0,0 +1,71 @@
+#include "MagicAmaroFilter.h"
+#include "src/main/cpp/utils/OpenglUtils.h"
+
+#define LOG_TAG "MagicAmaroFilter"
+#define ALOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
+#if DEBUG
+#define ALOGV(...) __android_log_print(ANDROID_LOG_VERBOSE, LOG_TAG, __VA_ARGS__)
+#else
+#define ALOGV(...)
+#endif
+
+#define GET_ARRAY_LEN(array,len){len = (sizeof(array) / sizeof(array[0]));}
+
+/**
+ * cool滤镜
+ */
+
+MagicAmaroFilter::MagicAmaroFilter(){
+
+}
+
+MagicAmaroFilter::MagicAmaroFilter(AAssetManager *assetManager)
+    : GPUImageFilter(readShaderFromAsset(assetManager,"default_vertex.glsl"), readShaderFromAsset(assetManager,"amaro.glsl")){
+    GET_ARRAY_LEN(inputTextureHandles,len);
+}
+
+MagicAmaroFilter::~MagicAmaroFilter() {
+
+}
+
+void MagicAmaroFilter::onDestroy() {
+    glDeleteTextures(len,inputTextureHandles);
+    *inputTextureHandles={-0};
+}
+
+void MagicAmaroFilter::onDrawArraysPre() {
+    for (int i = 0; i < len; ++i) {
+        if (inputTextureHandles[i] != 0) {
+            glActiveTexture(static_cast<GLenum>(GL_TEXTURE3 + i));
+            glBindTexture(GL_TEXTURE_2D, inputTextureHandles[i]);
+            glUniform1i(inputTextureUniformLocations[i], 3);
+        }
+    }
+}
+
+void MagicAmaroFilter::onDrawArraysAfter() {
+    for (int i = 0; i < len; ++i) {
+        if (inputTextureHandles[i] != 0) {
+            glActiveTexture(static_cast<GLenum>(GL_TEXTURE3 + i));
+            glBindTexture(GL_TEXTURE_2D, inputTextureHandles[i]);
+            glActiveTexture(GL_TEXTURE0);
+        }
+    }
+}
+
+
+void MagicAmaroFilter::onInit() {
+    GPUImageFilter::onInit();
+    for (int i = 0; i < len; ++i) {
+        inputTextureUniformLocations[i] = glGetUniformLocation(mGLProgId,"inputImageTexture"+(2*i));
+        mGLStrengthLocation = glGetUniformLocation(mGLProgId,"strength");
+    }
+}
+
+void MagicAmaroFilter::onInitialized() {
+    GPUImageFilter::onInitialized();
+    glUniform1f(mGLStrengthLocation, 1.0f);
+    inputTextureHandles[0] = loadTextureFromAssets(assetManager,"brannan_blowout.png");
+    inputTextureHandles[1] = loadTextureFromAssets(assetManager,"overlaymap.png");
+    inputTextureHandles[2] = loadTextureFromAssets(assetManager,"amaromap.png");
+}
