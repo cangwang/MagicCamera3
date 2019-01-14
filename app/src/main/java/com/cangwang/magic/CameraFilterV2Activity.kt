ab3fb0cc6f319311fbce0743cb67578f096c12d4
cangwang <zengjiale@yy.com>
2019-01-14 21:13:27 +0800

    [2019.1.14]减少无用代码

--- app/src/main/java/com/cangwang/magic/CameraFilterV2Activity.kt
+++ app/src/main/java/com/cangwang/magic/CameraFilterV2Activity.kt
@@ -1,32 +1,21 @@
 package com.cangwang.magic
 
-import android.Manifest
 import android.animation.Animator
 import android.animation.AnimatorListenerAdapter
 import android.animation.ObjectAnimator
 import android.app.AlertDialog
-import android.content.DialogInterface
 import android.content.pm.ActivityInfo
-import android.content.pm.PackageManager
 import android.graphics.Point
-import android.hardware.Camera
-import android.os.Build
 import android.os.Bundle
-import android.os.Environment
-import android.support.v4.app.ActivityCompat
-import android.support.v4.content.PermissionChecker
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
-import android.view.SurfaceHolder
 import android.view.View
 import android.view.Window
 import android.view.WindowManager
 import android.widget.RelativeLayout
 import com.cangwang.magic.adapter.FilterAdapter
 import com.cangwang.magic.camera.CameraCompat
-import com.cangwang.magic.util.CameraHelper
 import com.cangwang.magic.util.OpenGLJniLib
-import com.cangwang.magic.view.CameraFilterSurfaceCallback
 import com.cangwang.magic.view.CameraFilterSurfaceCallbackV2
 import kotlinx.android.synthetic.main.activity_camera.*
 import kotlinx.android.synthetic.main.filter_layout.*
@@ -40,17 +29,12 @@ class CameraFilterV2Activity:AppCompatActivity(){
     private val MODE_PIC = 1
     private val MODE_VIDEO = 2
     private var mode = MODE_PIC
-    private var CAMERA_PERMISSION_REQ = 1
-    private var STORGE_PERMISSION_REQ = 2
+
     private var mAdapter: FilterAdapter? = null
     private var mSurfaceCallback:CameraFilterSurfaceCallbackV2?=null
     private var beautyLevel:Int = 0
 
     var mCamera: CameraCompat?=null
-    private val ASPECT_RATIO_ARRAY = floatArrayOf(9.0f / 16, 3.0f / 4)
-    var mAspectRatio = ASPECT_RATIO_ARRAY[0]
-
-    var mCameraId = Camera.CameraInfo.CAMERA_FACING_BACK
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -59,26 +43,11 @@ class CameraFilterV2Activity:AppCompatActivity(){
         requestWindowFeature(Window.FEATURE_NO_TITLE)
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
         setContentView(R.layout.activity_camera)
-//        if (PermissionChecker.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED) run {
-//            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA),CAMERA_PERMISSION_REQ)
-//        }else {
-            initView()
-            initCamera()
-//        }
-//        if (PermissionChecker.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) run {
-//            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),STORGE_PERMISSION_REQ)
-//        }
+        initView()
+        initCamera()
+
     }
 
-//    private val types = arrayOf(MagicFilterType.NONE, MagicFilterType.FAIRYTALE, MagicFilterType.SUNRISE,
-//            MagicFilterType.SUNSET, MagicFilterType.WHITECAT, MagicFilterType.BLACKCAT, MagicFilterType.SKINWHITEN, MagicFilterType.HEALTHY,
-//            MagicFilterType.SWEETS, MagicFilterType.ROMANCE, MagicFilterType.SAKURA, MagicFilterType.WARM, MagicFilterType.ANTIQUE,
-//            MagicFilterType.NOSTALGIA, MagicFilterType.CALM, MagicFilterType.LATTE, MagicFilterType.TENDER, MagicFilterType.COOL,
-//            MagicFilterType.EMERALD, MagicFilterType.EVERGREEN, MagicFilterType.CRAYON, MagicFilterType.SKETCH, MagicFilterType.AMARO,
-//            MagicFilterType.BRANNAN, MagicFilterType.BROOKLYN, MagicFilterType.EARLYBIRD, MagicFilterType.FREUD, MagicFilterType.HEFE, MagicFilterType.HUDSON,
-//            MagicFilterType.INKWELL, MagicFilterType.KEVIN, MagicFilterType.LOMO, MagicFilterType.N1977, MagicFilterType.NASHVILLE,
-//            MagicFilterType.PIXAR, MagicFilterType.RISE, MagicFilterType.SIERRA, MagicFilterType.SUTRO, MagicFilterType.TOASTER2,
-//            MagicFilterType.VALENCIA, MagicFilterType.WALDEN, MagicFilterType.XPROII)
     private val types = OpenGLJniLib.getFilterTypes()
 
     fun initView(){
@@ -102,7 +71,6 @@ class CameraFilterV2Activity:AppCompatActivity(){
         }
 
         btn_camera_switch.setOnClickListener {
-//            switchCamera()
             mCamera?.switchCamera()
         }
 
@@ -149,25 +117,7 @@ class CameraFilterV2Activity:AppCompatActivity(){
     override fun onDestroy() {
         mCamera?.stopPreview(true)
         mSurfaceCallback?.releaseOpenGL()
-//        releaseCamera()
         super.onDestroy()
-    }
-
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-//        if (grantResults.size !=1 || grantResults[0] ==PackageManager.PERMISSION_GRANTED){
-//            if (mode == MODE_PIC){
-//                takePhoto()
-//            }else{
-//                takeVideo()
-//            }
-//        }
-//        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-//        if (requestCode == CAMERA_PERMISSION_REQ &&(grantResults.size != 1 || grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
-//            initView()
-//            initCamera()
-//        } else {
-//            super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-//        }
     }
 
     fun takePhoto(){
@@ -177,39 +127,6 @@ class CameraFilterV2Activity:AppCompatActivity(){
     fun takeVideo(){
 
     }
-
-//    fun switchCamera(){
-//        mCameraId = if (mCameraId == Camera.CameraInfo.CAMERA_FACING_BACK ) Camera.CameraInfo.CAMERA_FACING_FRONT else Camera.CameraInfo.CAMERA_FACING_BACK
-//        releaseCamera()
-//        mCamera = openCamera(glsurfaceview_camera.holder)
-//        mSurfaceCallback?.changeCamera(mCamera)
-//    }
-//
-//    fun releaseCamera(){
-//        mCamera?.setPreviewCallback(null)
-//        mCamera?.stopPreview()
-//        mCamera?.release()
-//        mCamera = null
-//    }
-//
-//
-//    fun openCamera(holder: SurfaceHolder?):Camera?{
-//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-//            return null
-//        }
-//
-//        if (mCamera!=null){
-//            return mCamera
-//        }
-//        mCamera = CameraHelper.openCamera(mCameraId)
-//
-//        mCamera?.let {
-//            //这里android 相机长和宽默认偏移90度，所以传入要对调
-//            CameraHelper.setOptimalSize(it,mAspectRatio,CameraHelper.getScreenHeight(),CameraHelper.getScreenWidth())
-//            CameraHelper.setDisplayOritation(this,it,mCameraId)
-//        }
-//        return mCamera
-//    }
 
     private fun showFilters() {
         val animator = ObjectAnimator.ofInt(layout_filter, "translationY", layout_filter.height, 0)
