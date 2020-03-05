b0dde30ea0485238af95547df84a744fc02a2376
Cang Wang <cangwang2116@gmail.com>
2020-03-05 23:13:32 +0800

    [2020.3.5]帧代码提交

--- filter/src/main/cpp/filter/source/Framebuffer.hpp
+++ filter/src/main/cpp/filter/source/Framebuffer.hpp
@@ -11,14 +11,62 @@
 #include <GLES3/gl3ext.h>
 #endif
 
-#include <vecotr>
+#include <vector>
 #include "Ref.hpp"
 
 /**
  * cangwang 2020.3.5
  */
-class Framebuffer : public Ref {
+NS_GI_BEGIN
 
+typedef struct {
+   GLenum minFilter;
+   GLenum magFilter;
+   GLenum wrapS;
+   GLenum wrapT;
+   GLenum internalFormat;
+   GLenum format;
+   GLenum type;
+} TextureAttributes;
+
+//帧缓存对象
+class Framebuffer : public Ref {
+public:
+    Framebuffer(int width, int height, bool onlyGenerateTexture = false,
+            const TextureAttributes textureAttributes = defaultTextureAttributes);
+    ~Framebuffer();
+
+    virtual void release(bool retureToCache = true);
+
+    GLuint getTexture() const {
+        return _texture;
+    }
+
+    GLuint getFramebuffer() const {
+        return _framebuffer;
+    }
+
+    int getWidth() const { return _width; }
+    int getHeight() const { return _height; }
+    const TextureAttributes& getTextureAttributes() const { return _textureAttributes;}
+    bool hasFramebuffer() { return _hasFB; }
+
+    void active();
+    void inactive();
+
+    static TextureAttributes defaultTextureAttributes;
+private:
+    int _width, _height;
+    TextureAttributes _textureAttributes;
+    bool _hasFB;
+    GLuint _texture;
+    GLuint _framebuffer;
+    void _generateTexture();
+    void _generateFramebuffer();
+
+    static std::vector<Framebuffer*> _framebuffers;
 };
 
+
+NS_GI_END
 #endif
--- filter/src/main/cpp/filter/source/Target.hpp
+++ filter/src/main/cpp/filter/source/Target.hpp
@@ -2,11 +2,28 @@
 #define Target_hpp
 
 #include "marcos.hpp"
-#include <>
+#include <map>
 
 /**
  * cangwang 2020.3.5
  */
+NS_GI_BEGIN
 
+enum RotationMode {
+    NoRotation = 0,
+    RotateLeft,
+    RotateRight,
+    FlipVertical,
+    FlipHorizontal,
+    RotateRightFlipVertical,
+    RotateRightFlipHorizontal,
+    Rotate180
+};
+
+class Target: public virtual Ref {
+
+};
+
+NS_GI_END
 
 #endif
